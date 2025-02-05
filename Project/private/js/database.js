35f4e278ce7f356c9f3ffd3d941d767a61b13cbc
Jeck0v <arnaud.fischer75@gmail.com>
2025-02-05 22:43:43 +0100

    :seedling: add .env

--- Project/private/js/database.js
+++ Project/private/js/database.js
@@ -4,19 +4,18 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup #available-libraries
 
 // Your web app's Firebase configuration
+require('dotenv').config();
+
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 };
 
 // initalize firebase
 const firebase = initializeApp(firebaseConfig);
-
-
-
 export{firebase}
